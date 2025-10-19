5cfe2d26bf5a95c645bd250ff6aa422a15f59ec9
dmitryemacs <dimadimof81@gmail.com>
2025-10-19 11:18:20 +0300

    update emacs config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -24,17 +24,21 @@
 
 ;; Custom cursor face
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "white")))))
 
 ;; Packages with use-package
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode 1))
+
 (use-package ivy
   :ensure t
   :init
   (ivy-mode 1))
+
+(use-package nix-mode
+  :ensure t)
 
 (use-package swiper
   :ensure t
@@ -66,6 +70,20 @@
                          (interactive)
                          (c-toggle-comment-style -1)))
 
+
+;; compilation
+(defun my/compilation-setup ()
+  (setq compilation-window-height 20)
+  (setq display-buffer-alist
+        '(("\\*compilation\\*"
+           (display-buffer-below-selected)
+           (window-height . fit-window-to-buffer)))))
+
+(add-hook 'compilation-mode-hook 'my/compilation-setup)
+
+;; map
+(global-set-key (kbd "C-c c") 'compile)
+
 ;; Ensure packages are installed
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -77,5 +95,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(gruber-darker))
- '(package-selected-packages '(ace-window gruber-darker-theme magit swiper)))
-
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     "9c6aa7eb1bde73ba1142041e628827492bd05678df4d9097cda21b1ebcb8f8b9"
+     default))
+ '(package-selected-packages
+   '(ace-window company gptel gruber-darker-theme magit
+		nix-mode swiper)))
