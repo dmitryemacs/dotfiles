f3ca59c0a60169b5b242dab4cb01dfea5c67ad80
dmitryemacs <dimadimof81@gmail.com>
2026-07-02 20:37:43 +0300

    enable mouse and true-color support in tmux

--- darwinflake/home-manager/programs/tmux.nix
+++ darwinflake/home-manager/programs/tmux.nix
@@ -5,5 +5,10 @@
     shortcut = "a";
     clock24 = true;
     historyLimit = 5000;
+    mouse = true;
+    extraConfig = ''
+      set -g default-terminal "tmux-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+    '';
   };
 }
