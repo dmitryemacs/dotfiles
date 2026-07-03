97075b7275e4a8395bdd7c234c1d4d6ac0c27774
dmitryemacs <dimadimof81@gmail.com>
2026-07-03 21:17:24 +0300

    wofi: remove prompt icon, use Hack Nerd Font, add wofi to home packages

--- nixflake/modules/wofi.nix
+++ nixflake/modules/wofi.nix
@@ -1,6 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = [ pkgs.wofi ];
+
   home.file = {
     ".config/wofi/config".source = ../config/wofi/config;
     ".config/wofi/style.css".source = ../config/wofi/style.css;
