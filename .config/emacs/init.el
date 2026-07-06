cb8f2b3341053e514223e02cdf66eb92082b3374
dmitryemacs <dimadimof81@gmail.com>
2026-07-06 08:08:34 +0300

    add doom-like keybindings: general.el, which-key, counsel, projectile

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -67,7 +67,16 @@
 (use-package ivy
   :ensure t
   :init
-  (ivy-mode 1))
+  (ivy-mode 1)
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
+
+(use-package counsel
+  :ensure t
+  :after ivy
+  :config
+  (counsel-mode 1))
 
 (use-package swiper
   :ensure t
@@ -116,6 +125,56 @@
   :after evil
   :config
   (evil-collection-init '(org magit)))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode 1))
+
+(use-package general
+  :ensure t
+  :after evil
+  :config
+  (general-evil-setup)
+
+  (general-create-definer leader
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (leader
+    :keymaps 'override
+    "." '(counsel-find-file :which-key "find file")
+    "SPC" '(counsel-M-x :which-key "M-x")
+    ";" '(counsel-imenu :which-key "imenu")
+    "'" '(projectile-switch-project :which-key "projectile")
+    "b b" '(ivy-switch-buffer :which-key "switch buffer")
+    "b d" '(kill-current-buffer :which-key "kill current buffer")
+    "b k" '(kill-buffer :which-key "kill buffer")
+    "f f" '(counsel-find-file :which-key "find file")
+    "f r" '(counsel-recentf :which-key "recent files")
+    "f s" '(save-buffer :which-key "save buffer")
+    "p p" '(projectile-switch-project :which-key "switch project")
+    "p f" '(projectile-find-file :which-key "find file in project")
+    "w v" '(split-window-right :which-key "split right")
+    "w s" '(split-window-below :which-key "split below")
+    "w d" '(delete-window :which-key "delete window")
+    "w o" '(delete-other-windows :which-key "delete other windows")
+    "w h" '(evil-window-left :which-key "window left")
+    "w j" '(evil-window-down :which-key "window down")
+    "w k" '(evil-window-up :which-key "window up")
+    "w l" '(evil-window-right :which-key "window right")
+    "w w" '(other-window :which-key "other window")
+    "q q" '(save-buffers-kill-terminal :which-key "quit emacs")
+    "t t" '(toggle-truncate-lines :which-key "truncate lines")
+    "t n" '(display-line-numbers-mode :which-key "line numbers")))
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode 1)
+  :config
+  (setq projectile-completion-system 'ivy))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (custom-set-variables
@@ -128,4 +187,4 @@
    '("a7b0f6df966667c8601d252ef42b76e2a3508e7ef8ccad801c80e2a6a2537497"
      default))
  '(package-selected-packages
-             '(ace-window company d-mode doom-themes evil evil-collection magit org org-modern swiper yasnippet-snippets)))
+             '(ace-window company counsel d-mode doom-themes evil evil-collection general magit org org-modern projectile swiper which-key yasnippet-snippets)))
